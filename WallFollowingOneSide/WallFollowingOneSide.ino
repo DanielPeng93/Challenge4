136fe1dd4f746b2988efbfd3d0bf75b76d984ad8
wreevehicks <wrhicks@bu.edu>
2015-10-22 17:47:29 -0400

    Code for Using Two LIDARs and Wall Following with One Sensor
    Two LIDAR code works until driving code is implemented - may need to be
    processed on separate microcontroller.
    Wall following code - decreasing oscillations, need heading information
    to improved corner ability.

--- WallFollowingOneSide/WallFollowingOneSide.ino
+++ WallFollowingOneSide/WallFollowingOneSide.ino
@@ -6,6 +6,7 @@
  */
 
 //#include <I2C.h>
+#include <Servo.h>
 #include <Wire.h>
 #include <PID_v1.h>
 #define    LIDARLite_ADDRESS   0x62          // Default I2C Address of LIDAR-Lite.
@@ -13,8 +14,17 @@
 #define    MeasureValue        0x04          // Value to initiate ranging.
 #define    RegisterHighLowB    0x8f          // Register to get both High and Low bytes in 1 call.
 
+Servo wheels; // servo for turning the wheels
+Servo esc; // not actually a servo, but controlled like one!
+bool startup = true; // used to ensure startup only happens once
+int startupDelay = 1000; // time to pause at each calibration step
+double maxSpeedOffset = 45; // maximum speed magnitude, in servo 'degrees'
+double maxWheelOffset = 75; // maximum wheel turn magnitude, in servo 'degrees'
+
+
 int last_0;
 int last_1;
+int last_PID0;
 
 int distance;
 
@@ -25,13 +35,20 @@ int active_sensor = 0; //0 is left, 1 is right
 
 // define variables we will be connecting
 double Setpoint, Input, Output;
-int Kp = 2;     // initial 2
-int Ki = 0.05;  // initial 0.05
-int Kd = 0.5;   // initial 0.5
+int Kp = 1.5;     // initial 2
+int Ki = 0.00;  // initial 0.05
+int Kd = 0.9;   // initial 0.5
 
-PID PIDleft(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+PID PID0(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup() {
+
+  wheels.attach(8); // initialize wheel servo to Digital IO Pin #8
+  esc.attach(9); // initialize ESC to Digital IO Pin #9
+  /*  If you're re-uploading code via USB while leaving the ESC powered on,
+   *  you don't need to re-calibrate each time, and you can comment this part out.
+   */
+  calibrateESC();
 
   //initialize LIDAR's left on, right off
   pinMode(switch_0, OUTPUT);
@@ -44,31 +61,53 @@ void setup() {
   delay(100); // Waits to make sure everything is powered up before sending or receiving data
 
   //initialize linked variables
-  Setpoint = 30; //30
-  PIDleft.SetOutputLimits(-50, 50);
+  Setpoint = 100; //100
+  PID0.SetOutputLimits(-50, 50);
 
   //turn on PID loop
-  PIDleft.SetMode(AUTOMATIC);
+  PID0.SetMode(AUTOMATIC);
 
 }
 
+
+/* Convert degree value to radians */
+double degToRad(double degrees) {
+  return (degrees * 71) / 4068;
+}
+
+/* Convert radian value to degrees */
+double radToDeg(double radians) {
+  return (radians * 4068) / 71;
+}
+
+/* Calibrate the ESC by sending a high signal, then a low, then middle.*/
+void calibrateESC() {
+  esc.write(180); // full backwards
+  delay(startupDelay);
+  esc.write(0); // full forwards
+  delay(startupDelay);
+  esc.write(90); // neutral
+  delay(startupDelay);
+  esc.write(90); // reset the ESC to neutral (non-moving) value
+}
+
 void loop() {
-  for (int i = 0; i < 2; i++) {
+  for (int i = 0; i < 1; i++) {
     //    active_sensor = i;
     switch (i) {
       case 0:
         digitalWrite(switch_0, LOW);
         digitalWrite(switch_1, LOW);
-        delay(10);
+        delay(5);
         digitalWrite(switch_0, HIGH);
-        delay(10);
+        delay(5);
         break;
       case 1:
         digitalWrite(switch_0, LOW);
         digitalWrite(switch_1, LOW);
-        delay(10);
+        delay(5);
         digitalWrite(switch_1, HIGH);
-        delay(10);
+        delay(5);
         break;
     }
     //distance array to average distances
@@ -100,25 +139,25 @@ void loop() {
       //low pass filter
       switch (i) {
         case 0:
-          if (distance < 0 || distance > 400) {
+          if (distance < 0 || distance > 4000) {
             distance = last_0;
           }
           distance = 0.7 * distance + 0.3 * last_0;
           last_0 = distance;
           break;
         case 1:
-          if (distance < 0 || distance > 400) {
+          if (distance < 0 || distance > 4000) {
             distance = last_1;
           }
           distance = 0.7 * distance + 0.3 * last_1;
           last_1 = distance;
           break;
       }
-    distSum = distSum + distance;
-    Serial.print(j);
-    Serial.print(" : ");
-    Serial.println(distSum);
-    delay(10);
+      distSum = distSum + distance;
+      Serial.print(j);
+      Serial.print(" : ");
+      Serial.println(distance);
+      delay(5);
     }
 
     distance = distSum / 5;
@@ -127,8 +166,13 @@ void loop() {
     Input = distance;
 
     // use PID loop
-    PIDleft.Compute();
-    delay(10);
+    PID0.Compute();
+    delay(5);
+
+    Output = 0.9 * Output + 0.1 * last_PID0;
+
+    wheels.write(90-15);
+    esc.write(90 + Output);
 
     // Print Distance
     switch (i) {
@@ -145,6 +189,7 @@ void loop() {
         Serial.print(", PID1: ");
         Serial.println(Output);
     }
-    delay(500);
+    delay(10);
   }
+  delay(10);
 }
