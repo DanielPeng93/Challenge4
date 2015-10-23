b87fb91eb7f45d159c6baa012486c84b709f81be
DanielPeng93 <xinpeng@bu.edu>
2015-10-23 12:06:08 -0400

    Update UltrasonicHCSR04NewPing.ino

--- UltraSonicRangeFinder/UltraSonicHC-Sr04/NewPing/UltrasonicHCSR04NewPing.ino
+++ UltraSonicRangeFinder/UltraSonicHC-Sr04/NewPing/UltrasonicHCSR04NewPing.ino
@@ -2,7 +2,7 @@
  
 #define TRIGGER_PIN 12
 #define ECHO_PIN 11
-#define MAX_DISTANCE 300
+#define MAX_DISTANCE 1000
  
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
  
@@ -13,6 +13,10 @@ void setup() {
 void loop() {
    delay(50);
    unsigned int uS = sonar.ping_cm();
+   //Between 3 meters - 10 meters, just assume it is 3 meters
+   if(uS> 300){
+    uS = 300;
+   }
    Serial.print(uS);
    Serial.println("cm");
 }
