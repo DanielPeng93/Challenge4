72e2935e233f278548c5d9e31238b58d4aacb05b
Hao Wu <iamwuhao@bu.edu>
2015-10-28 15:57:30 -0400

    3_point_turn

--- _3_point_turn/_3_point_turn.ino
+++ _3_point_turn/_3_point_turn.ino
@@ -1,14 +1,11 @@
 #include <Servo.h>
 
 Servo wheels; // servo for turning the wheels
-Servo esc; // not actually a servo, but controlled like one!
+Servo esc; // servo for motors
+
 double IROffset = 20;
 
 float IRFront, IRBack, cmFront, cmBack;
-
-// twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
 
 void setup() {
   Serial.begin(9600);
@@ -17,46 +14,43 @@ void setup() {
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
 
-  myservo.write(0); //initialization
 }
 
 void loop() {
     IRFront = analogRead(A1);
-    IRBack  = analogRead(A2);
-    
     cmFront = 10650.08 * pow(IRFront, -0.935) - 10;
+    IRBack  = analogRead(A2);
     cmBack = 10650.08 * pow(IRBack, -0.935) - 10;
     
-    if(cmFront > 150)
+    if(cmFront > 150){
       Serial.println("Out of range.");
+      esc.write(0);
+      wheels.write(90);
+    }
     else {
-      threePointTurn(cmFront,cmBack);    
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    esc.write(0);
-    wheels.write(90);
-    delay(1);
-}
-
-
-// calculate the x, y position of the object
-void threePointTurn(float f, float b) {
-    //make some difference between 2 lidar
-    wheels.write(0);
-    esc.write(80);
-    delay(100);
-    
-    while(f > IROffset){// and two lidar value not equals
-      wheels.write(0); //turn left
-      esc.write(80); //slow forward
-    }
-    while(b > IROffset){// and two lidar value not equals 
-      wheels.write(180);//turn right
-      esc.write(100);//slow backward  
-    }
-    while(true){// lidar find the car is parallel to the wall
       wheels.write(0);
       esc.write(80);
+      delay(100);
+    
+    while(cmFront > IROffset){// and two lidar value not equals
+      IRFront = analogRead(A1);
+      cmFront = 10650.08 * pow(IRFront, -0.935) - 10;
+      wheels.write(0); //turn left
+      esc.write(0); 
+    }
+    while(cmBack > IROffset){// and two lidar value not equals 
+      IRBack  = analogRead(A2);
+      cmBack = 10650.08 * pow(IRBack, -0.935) - 10;
+      wheels.write(180);//turn right
+      esc.write(180);  
+    }
+    while(true){// lidar find the car is parallel to the wall
+      IRFront = analogRead(A1);
+      cmFront = 10650.08 * pow(IRFront, -0.935) - 10;
+      wheels.write(0);
+      esc.write(0);
     }
   }
+
+}
 
