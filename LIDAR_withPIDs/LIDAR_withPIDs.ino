38d353899cdaee1c2d9af16d678a2b71ac53e2c8
ghirlekar <gaurav.hirlekar@gmail.com>
2015-10-29 11:53:41 -0400

    Added wheel encoder code and ISR and also xbee button to start/stop crawler

--- LIDAR_withPIDs/LIDAR_withPIDs.ino
+++ LIDAR_withPIDs/LIDAR_withPIDs.ino
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <PID_v1.h>
 #include <Servo.h>
+#include <Xbee.h>
+#include "XbeeApiStream.h"
 
 #define LIDARLITE_ADDR 0x62
 #define LIDARLITE_CMD_CTRL_ADDR 0x00
@@ -23,20 +25,14 @@ int state_cycle_count = 0;
 double dist[2];
 
 //set up servos
-Servo steeringServo;
-Servo motorServo;
-const int steeringPin = 9;
-const int motorPin = 8;
+Servo steeringServo, motorServo;
+const int steeringPin = 9, motorPin = 8;
 
 //define PID variables
-double Setpoint_theta, Input_theta, Output_theta, Setpoint_dist, Input_dist, Output_dist;
+double Setpoint_theta = 0, Input_theta, Output_theta, Setpoint_dist = 90, Input_dist, Output_dist;
 //specify initial PID links and tuning
-int KpT = 1.7;    //1.7
-int KiT = 0;      //0
-int KdT = 0.01;   //0.01
-int KpD = 1.1;      //1
-int KiD = 0.075;    //0
-int KdD = 0.97;   //0
+int KpT = 1.7, KiT = 0, KdT = 0.01;  // {1.7, 0, 0.01}
+int KpD = 1.1, KiD = 0.075, KdD = 0.97;   // {1, 0, 0}
 PID PID_theta(&Input_theta, &Output_theta, &Setpoint_theta, KpT, KiT, KdT, DIRECT);
 PID PID_dist(&Input_dist, &Output_dist, &Setpoint_dist, KpD, KiD, KdD, DIRECT);
 
@@ -46,14 +42,18 @@ double LIDARspacing = 22.5;
 // Set pi
 const float pi = 3.142;
 
+// Wheel encoder pulse counter variable
+const int encoder_pin = 2;
+volatile int encoder_count = 0;
+double wheel_rpm;
+#define ENCODER_SCALING 1
+
+XbeeApiStream xbeeStream = XbeeApiStream();
+bool state_paused = true;
+
+
 //---------------------------------------------------------//
 void setup() {
-  // Initialize linked variables
-  Input_theta = 0;
-  Input_dist = 100;
-  Setpoint_theta = 0;
-  Setpoint_dist = 90;
-
   PID_theta.SetMode(AUTOMATIC);
   PID_dist.SetMode(AUTOMATIC);
 
@@ -65,12 +65,19 @@ void setup() {
   motorServo.attach(motorPin);
 
   // Begin serial
-  Serial.begin(57600);
-  Serial.println("Serial begin");
+  Serial.begin(9600);
+  xbeeStream.begin(Serial);
+  xbeeStream.write("Serial begin");
   // Open and join irc bus as master
   Wire.begin();
   delay(100);
 
+  // Set encoder pin as input and enable internal pull-up resistor
+  pinMode(encoder_pin, INPUT);
+  digitalWrite(encoder_pin, HIGH);
+  attachInterrupt(digitalPinToInterrupt(encoder_pin), encoder_ISR, CHANGE);
+
+  // Set up the lidar pins and change the lidar acquisition count register value
   for (int i = 0; i < 2; i++)   pinMode(lidar_pwr_en[i], OUTPUT);
   for (int i = 0; i < 2; i++) {
     for (int j = 0; j < 2; j++)  digitalWrite(lidar_pwr_en[j], LOW);
@@ -86,30 +93,29 @@ void setup() {
 }
 
 void loop() {
-  now = millis();
-  read_lidars();
-  if (now - timestamp > 40) {
-    timestamp = now;
-//    dist[0] = 4.5 + dist[0] * (0.98); //scaling equation
-    Input_theta = calcAngle(dist[0], dist[1]);
-    Input_dist = calcPerpDist(dist[0], dist[1], Input_theta);
-    Input_theta = Input_theta * 180 / pi;
-//    Setpoint_theta = constrain(0.5*(Input_dist - Setpoint_dist), -45, 45);
+  if(xbeeStream.available() && xbeeStream.read() == 0x00)  state_paused = !state_paused;
+  if (!state_paused) {
+    now = millis();
+    read_lidars();
+    if (now - timestamp > 40) {
+      wheel_rpm = ENCODER_SCALING * encoder_count / (now - timestamp);
+      encoder_pin = 0;
+      timestamp = now;
+      //    dist[0] = 4.5 + dist[0] * (0.98); //scaling equation
+      Input_theta = calcAngle(dist[0], dist[1]);
+      Input_dist = calcPerpDist(dist[0], dist[1], Input_theta);
+      Input_theta = Input_theta * 180 / pi;
+      //    Setpoint_theta = constrain(0.5*(Input_dist - Setpoint_dist), -45, 45);
 
-    // Run the PID loops
-    Serial.print(dist[0]);
-    Serial.print(", ");
-    Serial.print(dist[1]);
-    Serial.print(", ");
-    Serial.print(Input_theta);
-    Serial.print(", ");
-    Serial.println(Input_dist);
-    PID_theta.Compute();
-    PID_dist.Compute();
+      // Run the PID loops
+      xbeeStream.write(String(String(dist[0], 1) + ',' + String(dist[1], 1) + ',' + String(Input_theta, 1) + ',' + String(Input_dist, 1)));
+      PID_theta.Compute();
+      PID_dist.Compute();
 
-    motorServo.write(90  - 20 );
-    steeringServo.write(90 + (Output_theta) - Output_dist); // + Output_theta - Output_dist
-  } // (50/abs(Input_dist - Setpoint_dist)) * 
+      motorServo.write(90  - 20 );
+      steeringServo.write(90 + (Output_theta) - Output_dist); // + Output_theta - Output_dist
+    } // (50/abs(Input_dist - Setpoint_dist)) *
+  }
   delay(2);
 }
 
@@ -122,6 +128,10 @@ double calcAngle(double dist0, double dist1) {
 
 double calcPerpDist(double dist0, double dist1, double angle) {
   return ((dist0 + dist1) * cos(angle)) / 2;
+}
+
+void encoder_ISR() {
+  encoder_count++;
 }
 
 
@@ -151,7 +161,7 @@ void read_lidars() {
       if (Wire.requestFrom(LIDARLITE_ADDR, 2) >= 2) {
         uint16_t val = Wire.read() << 8 | Wire.read();
         if (val < 500 && val > 10) dist[enabled_lidar] = 0.2 * dist[enabled_lidar] + 0.8 * val;
-       
+
         enabled_lidar = 1 - enabled_lidar;
         lidar_state = 0;
       } else state_cycle_count++;
